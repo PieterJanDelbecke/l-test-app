77d2c5583d17d86d4aeebf84cb33784095803d07
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-03-12 11:58:45 +1100

    initial displaying of mapped content retrieved from API

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -35,26 +35,25 @@ const Home = () => {
         ? console.log("cinemaWorld is here")
         : console.log("cinemaWorld is not here")}
 
-      {filmWorld.Provider && (
-        <div>
+      {filmWorld.Provider &&
+        filmWorld.Movies.map((movie, index) => {
+          return (
+            <div className="movies" key={index}>
+              <h3>{movie.Title}</h3>
+              <img src={movie.Poster} alt={movie.Title} />
+              <h5>{movie.Actors} </h5>
+              <h6> Price: ${movie.Price}</h6>
+              <hr />
+            </div>
+          );
+        })}
+      {/* <div>
           <h3>{filmWorld.Movies[1].Actors}</h3>
           <img
             src={filmWorld.Movies[1].Poster}
             alt={filmWorld.Movies[1].Title}
           />
-        </div>
-      )}
-      {/* {filmWorld.Movies.map((movie, index) => {
-                return (
-                    <div className= "movies" key={index}>
-                        <h3>{movie.Title}</h3>
-                        <img src={movie.Poster} alt={movie.Title} />
-                        <h5>{movie.Actors} </h5>
-                        <h6> Price: ${movie.Price}</h6>
-                        <hr/>
-                    </div>
-                )
-            })} */}
+        </div> */}
     </>
   );
 };
